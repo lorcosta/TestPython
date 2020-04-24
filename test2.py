2e3556e06cfa6b9b9eeca1b2f3fb11bf4e6f44cd
Lorenzo <costa.lorenzo98@gmail.com>
2020-04-24 15:53:28 +0200

    Input utente

--- test2.py
+++ test2.py
@@ -1,2 +1,5 @@
 if __name__=='__main__':
-	print(f"2+3={2+3}")
+	name=input('Please write your name: ')
+	age=input('Please type your age: ')
+	height=input('Please type your height in centimetres: ')
+	print(f"So you are {name} and you're {age} years old and you're tall {height} cm...")
