54a3fe29eb1d4b950a30b22a0ca709d8ff852350
Lorenzo <costa.lorenzo98@gmail.com>
2020-04-24 15:48:58 +0200

    Operazioni aritmetiche in stampa

--- test2.py
+++ test2.py
@@ -1,7 +1,2 @@
 if __name__=='__main__':
-	nome='Lorenzo'
-	eta=22
-	dataNascita="21/05/1998"
-	print(f"My name is {nome} and I'm {eta} years old, I was born the {dataNascita}")
-
-	
+	print(f"2+3={2+3}")
