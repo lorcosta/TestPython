303007e5bb2c34ab467be60f401017e26aca0eb9
Lorenzo <costa.lorenzo98@gmail.com>
2020-04-24 17:07:41 +0200

    Dictionary (mappe)

--- test2.py
+++ test2.py
@@ -1,19 +1,15 @@
 if __name__=='__main__':
-	numbers=[1,2,3,4,5,6,7,8,9,10]
-	sommaNumeri=0
-	lunghezza=len(numbers)
-	for item in numbers:
-		sommaNumeri+=item
-	print(f'la media tra i numeri da 1 a 10 è {sommaNumeri/lunghezza}')
-	maxValue=0
-	for item in numbers:
-		if item>maxValue:
-			maxValue=item
-	print(f'Il valore massimo tra i numeri da 1 a 10 è {maxValue}')
-	minValue=100
-	for item in numbers:
-		if item<minValue:
-			minValue=item
-	print(f'Il valore minimo tra i numeri da 1 a 10 è {minValue}')
-	
-
+	name=input("Type your name: ")
+	surname=input("Type you surname: ")
+	place=input("What's your place of birth?: ")
+	birthday=input("When's your birthday?: ")
+	age=input("What's your age?: ")
+	personal_data={"Name":name,
+					"Surname":surname,
+					"Birth":{
+						"Place":place,
+						"Birthday":birthday
+					},
+					 "age":age
+					 }
+	print(f"Personal data: {personal_data['Name']} {personal_data['Surname']}, born in {personal_data['Birth']}, you are currently {personal_data['age']}")
